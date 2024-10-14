c2bc7a0ebd9debca60184c57a87e085de9396480
Jo Shepton <joshepton1@gmail.com>
2024-10-14 16:31:50 +0100

    updated first commit of udemy tutorial work

---
+++ csharp_experiments/udemy_tutorial_work/udemy_tutorial_work/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string myFriendsName;
+myFriendsName = "James";
+
+// use the variable name
+Console.WriteLine(myFriendsName);
+
+// overwrite the variable name
+myFriendsName = "Hannah";
+Console.WriteLine(myFriendsName);
+Console.ReadKey();
