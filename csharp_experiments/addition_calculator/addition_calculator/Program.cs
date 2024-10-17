af7f02fb74f856edbf304174a0c698391227fa29
Jo Shepton <joshepton1@gmail.com>
2024-10-17 16:26:21 +0100

    added addition functionality to take two user input numbers and returns their sum

---
+++ csharp_experiments/addition_calculator/addition_calculator/Program.cs
@@ -0,0 +1,10 @@
+﻿// takes user input and stores it
+// datatype is string, variable is userInput
+Console.WriteLine("Enter a number");
+var userInput = float.Parse(Console.ReadLine());
+Console.WriteLine("Enter another");
+var userInput2 = float.Parse(Console.ReadLine());
+
+float added_values  = userInput + userInput2;
+Console.WriteLine("These add up to " + added_values);
+Console.ReadKey();  
